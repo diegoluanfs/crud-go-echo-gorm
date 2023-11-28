daed9767cf8319b6c0b6a6f2a5763c4e12071d1c
Diego Silva <diegoluanfs@gmail.com>
2023-11-27 21:54:13 -0300

    feat: foram incluídas algumas funcionalidades da integração do coingecko

--- routes/routes.go
+++ routes/routes.go
@@ -4,6 +4,7 @@ import (
 	"crud-go-echo-gorm/controllers"
 	"crud-go-echo-gorm/repositories"
 	"crud-go-echo-gorm/services"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
@@ -19,6 +20,10 @@ func SetupRoutes(e *echo.Echo, db *gorm.DB) {
 	orderService := services.NewOrderService(orderRepository)
 	orderController := controllers.NewOrderController(orderService)
 
+	cryptoController := &controllers.CryptoController{
+		Client: &http.Client{},
+	}
+
 	// Configure as rotas para usuários
 	userGroup := e.Group("/api/v1/users")
 	userRoutes(userGroup, *userController)
@@ -26,6 +31,22 @@ func SetupRoutes(e *echo.Echo, db *gorm.DB) {
 	// Configure as rotas para ordens
 	orderGroup := e.Group("/api/v1/orders")
 	orderRoutes(orderGroup, orderController)
+
+	// Configure as rotas para cryptos
+	cryptoGroup := e.Group("/api/v1/coins")
+	cryptoRoutes(cryptoGroup, cryptoController)
+}
+
+func cryptoRoutes(g *echo.Group, cc *controllers.CryptoController) {
+	g.GET("/", cc.CryptoCoinList)
+	g.GET("/:id", cc.CryptoCoinInfo)
+	g.GET("/:id/market_chart", cc.CryptoCoinMarketChart)
+	g.GET("/status", cc.CryptoCoinStatus)
+	g.GET("/simple/price", cc.CryptoCoinSimplePrice)
+
+	//Adicionar endpoints referentes as operações dos usuários
+	g.GET("/transaction", cc.CryptoCoinTransaction)
+
 }
 
 func userRoutes(g *echo.Group, uc controllers.UserController) {
